925791175cacaf0dbad4715bb9a70081c914fae8
MAnsabRaza <ansabnasir153@gmail.com>
2026-01-11 21:47:15 +0500

    1

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -12,4 +11,5 @@ app.get("/test", (req, res) => {
   res.json({ message: "Laravel calling Node API successfully" });
 });
 
+// Export the Express app as a serverless function
 export default app;
--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import app from "./api/index.js";
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("Local server running at http://localhost:3000");
+  console.log(`Local server running at http://localhost:${PORT}`);
 });
