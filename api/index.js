4c364abf894ab22d938ef09d5b48afc3a4ce7c85
MAnsabRaza <ansabnasir153@gmail.com>
2026-01-11 21:04:56 +0500

    first commit

---
+++ api/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Vercel Node API running 🚀");
+});
+
+app.get("/test", (req, res) => {
+  res.json({ message: "Laravel calling Node API successfully" });
+});
+
+export default app;
